273acc26f9b408be5bb78842acf95c36dbe25774
Nuria <c.odena.nuria@gmail.com>
2022-01-04 17:41:21 +0100

    router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
-
+import { Fragment } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from "./components/AppNavbar";
-function App() {
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+
+const App =( ) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <AppNavbar />
-        <h1>hello there</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Fragment>
+        
+          <AppNavbar />
+          <div className="container">
+            
+            <Routes>
+          
+              <Route exact path="/" element={<Home/>}/>
+              <Route exact path="/about" element={<About />}/>
+            </Routes>
+            
+          </div>
+          
+        
+      </Fragment>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink} from 'reactstrap';
+import {Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, Container} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
  class AppNavbar extends Component {
      
@@ -18,17 +19,21 @@ import {Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink} fro
         return (
             <div>
                 <Navbar color="dark" dark expand="sm" className="mb-5">
-                    <div className="d-flex justify-content-between mx-5 w-100">
+                    <Container className="d-flex justify-content-between mx-5 w-100">
                         <NavbarBrand href="/">Mytinerary</NavbarBrand>
                         <NavbarToggler onClick={this.toggle}/>
-                        <Collapse isOpen={this.state.isOpen}>
+                        <Collapse isOpen={this.state.isOpen} navbar>
                             <Nav className="ml-auto" navbar>
-                                <NavItem>
-                                    <NavLink href='https://github.com/ncodena/mytinerary1.3'>Github</NavLink>
+                                <NavItem className="px-2">
+                                    <Link className="text-white mr-2" to='/'>Home</Link>
+                                </NavItem>
+
+                                <NavItem className="">
+                                <Link className="text-white px-2" to='/about'>About</Link>
                                 </NavItem>
                             </Nav>
                         </Collapse>
-                    </div>
+                    </Container>
 
                 </Navbar>
             </div>
